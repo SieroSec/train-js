eca10a00ab183999a55a1af74633ed5458808c26
Kuprescenka <sergejus.kuprescenka@solera.com>
2020-12-08 20:47:25 +0100

    Second commit

--- siero1.js
+++ siero1.js
@@ -1,34 +1,30 @@
-// console.log(_.capitalize("miau"));
-
 $(document).ready(function(){
-  // $("button").click(function(){$("p").hide();});
-  // $("#miau").click(function(){$("#miau").hide();});
-  // $("div.mur").click(function(){$("div.mur").hide();});
+  // let elem = ($("*").find("*"));
   let elem = ($("*").find("*"));
+  console.log(elem.length);
   console.log(elem);
-  // console.log(typeof(elem));
-
-  // console.log("Object keys: "+Object.keys(elem));
-  // console.log("Obect length: "+Object.keys(elem).length);
-  // console.log(elem["0"]);
-
-  for (i=0; i < elem.length; i++) {
-    console.log(elem[i]);
-    console.log(typeof(elem[i]));
-    console.log(Object.keys(elem[i]));
-    console.log(Object.prototype.valueOf(elem[i]));
-    console.log("-----");
-  }
-
+  // for (i=0; i < elem.length; i++) {
+  //   console.log(elem[i]);
+  // }
   
+  // play with jQuery ajax:
+  // $(selector).load(URL,data,callback);
+  // The required URL parameter specifies the URL you wish to load.
+  // The optional data parameter specifies a set of querystring key/value pairs to send along with the request.
+  // The optional callback parameter is the name of a function to be executed after the load() method is completed.
+  // responseTxt - contains the resulting content if the call succeeds
+  // statusTxt - contains the status of the call
+  // xhr - contains the XMLHttpRequest object
 
-  //var myJSON = JSON.stringify(elem);
-  // console.log(myJSON);
-  //  console.log(Object.keys(elem).forEach(item => {console.log(item)}));
+  $("button").click(function() {
+    $("div.ajax").load("ajax1.txt", function(responseTxt, statusTxt, xhr){
+      console.log(xhr);
+      if(statusTxt == "success")
+      console.log("STATUS: " + xhr.status + ", " + xhr.statusText);
+      if(statusTxt == "error")
+        console.log("STATUS: " + xhr.status + ", " + xhr.statusText);
+    });
+  })
 
-  $("button.hideall").click(function() {
-    //$("*").hide();
-    $(this).toggle();
-  });
 });
 
